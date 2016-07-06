f1a844f9f6f3b4f8dbcb28527899ae8863c998f5
nikolaynizruhin <lgnexus4fun@gmail.com>
2016-07-06 12:57:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Token-Based RESTful API
+Laravel 5.2 Token-Based Restful API
---
+++ app/Note.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'text', 'user_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'user_id'
+    ];
+}
